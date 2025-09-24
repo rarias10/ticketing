7a8b6eac2995826cd75b3a1521e76d4bcc99b5af
Rodrigo Arias <rarias10@gmail.com>
2025-09-24 14:16:17 -0600

    first commit

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,70 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var beforeAll: any;
+  var beforeEach: any;
+  var afterAll: any;
+  function signin(id?: string): string[];
+}
+
+jest.mock('../nats-wrapper');
+
+
+let mongo: any;
+
+
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+  
+  try {
+    await mongoose.connect(mongoUri);
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+    throw error;
+  }
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db?.collections();
+  if (collections) {
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
+
+global.signin = () => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com'
+  };
+
+  // Create a JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object - this is what cookie-session expects
+  const session = { jwt: token };
+
+  // Turn that into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // Return a cookie with the encoded data
+  return [`session=${base64}`];
+};
