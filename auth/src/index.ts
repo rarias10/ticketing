170596963d0e9a4de6fbdeefbf06bf4d5ceeb45d
Rodrigo Arias <rarias10@gmail.com>
2025-10-08 09:18:44 -0600

    added startup message to auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ const port = 3000;
 
 
 const start = async () => {
+  console.log('Starting up auth service...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
